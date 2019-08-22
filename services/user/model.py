1c11b76cc3cdf9fc94c4c02bdb0f0a10418486d7
jferragut <31458614+jferragut@users.noreply.github.com>
2019-08-22 16:49:35 -0400

    adjusted structure as well as added migrations

--- services/user/model.py
+++ services/user/model.py
@@ -1,16 +1,17 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __init__(**kwargs):
-        super(User, self).__init__(**kwargs)
+    
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "username": self.username,
+            "email": self.email
+        }
--- services/user/user.py
+++ services/user/user.py
@@ -4,14 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from models import user_model
+from services.user.model import db
+from services.user.model import User
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, user_model)
-app.init_app(app)
+MIGRATE = Migrate(app, db)
+db.init_app(app)
 CORS(app)
 
 
@@ -23,8 +24,10 @@ def sitemap():
 
 @app.route('/user', methods=["GET"])
 def user():
-    response_body = {"name": "Jon"}
-    return jsonify(response_body)
+    # response_body = {"name": "Jon"}
+    # return jsonify(response_body),200
+    return User
+
 
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
