8458b8db84ca929b8660469e38e69f483915fe47
jferragut <31458614+jferragut@users.noreply.github.com>
2019-08-25 01:56:26 -0400

    commented user db out for now; using catch-all #2

--- api/user/index.py
+++ api/user/index.py
@@ -1,22 +1,22 @@
 import os
 from flask import Flask, request, jsonify, url_for, Blueprint, Response
-from flask_dotenv import DotEnv
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_dotenv import DotEnv
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from api.user.model import db,User
+# from api.user.model import db,User
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.config.from_pyfile('settings.cfg')
-MIGRATE = Migrate(app, db)
-db.init_app(app)
-with app.app_context():
-    db.create_all()
-    db.session.commit()
-CORS(app)
+# app.url_map.strict_slashes = False
+# app.config.from_pyfile('settings.cfg')
+# MIGRATE = Migrate(app, db)
+# db.init_app(app)
+# with app.app_context():
+#     db.create_all()
+#     db.session.commit()
+# CORS(app)
 
 
 @app.route('/', defaults={'path': ''})
@@ -25,17 +25,17 @@ def catch_all(path):
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 
 
-@app.route('/api/user/', methods=["GET", "POST"])
-def user():
-    if request.method == 'POST':
-        request_json = request.get_json()
-        name = request_json.get('username')
-        email = request_json.get('email')
-        new = User(username=name, email=email)
-        db.session.add(new)
-        db.session.commit()
-        return 'User Added',200
-    else:
-        json_list = [i.serialize for i in User.query.all()]
-        return jsonify(json_list), 200
+# @app.route('/api/user/', methods=["GET", "POST"])
+# def user():
+#     if request.method == 'POST':
+#         request_json = request.get_json()
+#         name = request_json.get('username')
+#         email = request_json.get('email')
+#         new = User(username=name, email=email)
+#         db.session.add(new)
+#         db.session.commit()
+#         return 'User Added',200
+#     else:
+#         json_list = [i.serialize for i in User.query.all()]
+#         return jsonify(json_list), 200
 
