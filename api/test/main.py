8a927419d886e57ac34a2d5d8bd37ceae1ba1214
jferragut <31458614+jferragut@users.noreply.github.com>
2019-11-11 22:24:28 -0500

    cleaning out some un-needed files

--- api/test/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/test/', defaults={'path': ''})
-@app.route('/test/<path:path>')
-def catch_all(path):
-	return "You visited the %s page" % (path)
